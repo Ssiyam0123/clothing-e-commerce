3d1e996bcc73c522173b4cddd3b23471bd32e2f5
Ssiyam0123 <ssiyam0123@gmail.com>
2026-06-04 23:25:51 +0600

    fix: restore RootIndexRedirect to redirect directly to (tabs) on startup

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,23 +1,6 @@
 import { Redirect } from 'expo-router';
-import { useAuthStore } from '../store/authStore';
-import { View, ActivityIndicator } from 'react-native';
-import { brandColors } from '../constants/designSystem';
 
 export default function RootIndexRedirect() {
-  const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
-  const isLoading = useAuthStore((s) => s.isLoading);
-
-  if (isLoading) {
-    return (
-      <View className="flex-1 items-center justify-center bg-background">
-        <ActivityIndicator size="large" color={brandColors.primary} />
-      </View>
-    );
-  }
-
-  if (!isAuthenticated) {
-    return <Redirect href="/login" />;
-  }
-
   return <Redirect href="/(tabs)" />;
 }
+
